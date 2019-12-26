276102b5074a944e7cef5f35fcb1dfb163729a96
Deepak Kaul <deepak.b.kaul@gmail.com>
2019-12-25 20:49:02 -0500

    updating comments in interfaces

--- run/interface.go
+++ run/interface.go
@@ -5,18 +5,21 @@ import (
 )
 
 // Ingester provides interface for plugins that ingest data into reservoird
+// struct channel and wait group are for graceful shutdown
 type Ingester interface {
 	Config(string) error
 	Ingest(chan<- []byte, <-chan struct{}, *sync.WaitGroup) error
 }
 
 // Digester provides interface for plugins that filter/annotate data within reservoird
+// struct channel and wait group are for graceful shutdown
 type Digester interface {
 	Config(string) error
 	Digest(<-chan []byte, chan<- []byte, <-chan struct{}, *sync.WaitGroup) error
 }
 
 // Expeller provides interface for plugins that expel data outof reservoird
+// struct channel and wait group are for graceful shutdown
 type Expeller interface {
 	Config(string) error
 	Expel(<-chan []byte, <-chan struct{}, *sync.WaitGroup) error
