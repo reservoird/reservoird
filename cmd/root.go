9fc286836a984bc1a666c1eef48e1314680979cb
Deepak Kaul <deepak.b.kaul@gmail.com>
2020-01-29 21:56:00 -0500

    adding in address option

--- cmd/root.go
+++ cmd/root.go
@@ -4,6 +4,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var Address string
 var Debug bool
 var rootCmd = &cobra.Command{
 	Use:   "reservoird",
@@ -11,6 +12,7 @@ var rootCmd = &cobra.Command{
 }
 
 func init() {
+	rootCmd.PersistentFlags().StringVarP(&Address, "address", "a", ":5514", "host and port")
 	rootCmd.PersistentFlags().BoolVarP(&Debug, "debug", "d", false, "debug mode")
 }
 
